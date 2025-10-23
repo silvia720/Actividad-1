e9c24bc090a70f73d4444fa7ea6a4012ac535e72
silvia720 <silviamorillogil06@gmail.com>
2025-10-23 10:32:42 +0200

    Add files via upload

---
+++ boceto.js
@@ -0,0 +1,29 @@
+let borrarfondo = true //limpia cada frame
+let usarcolorA = true // alterna colores
+
+function setup() {
+  createCanvas(550, 550);
+  background(0, 22, 255); //color
+  noStroke() // sense borde
+}
+
+function draw() {
+  if(borrarfondo) background(255, 0, 0)w
+  //elegir color simple A/B
+  if(usarcolorA){
+    fill(0)
+  } else{
+    fill(150)
+  }
+  ellipse(mouseX, mouseY, 30, 30);
+}
+
+function mousePressed(){
+  usarcolorA = !usarcolorA
+}
+
+function keyPressed(){
+  if(key === 'b' || key === 'B'){
+    borrarfondo = !borrarfondo
+  }
+}
